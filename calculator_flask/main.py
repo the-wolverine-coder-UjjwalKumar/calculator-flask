e579c6ef64dd42cb7f3e51264106b19555475e9f
the_wolverine_coder <61937393+the-wolverine-coder-UjjwalKumar@users.noreply.github.com>
2021-03-29 01:34:05 +0530

    Added the files for calculator

---
+++ calculator_flask/main.py
@@ -0,0 +1,78 @@
+from flask import Flask, render_template, request
+
+Flask_App = Flask(__name__) # Creating our Flask Instance
+
+@Flask_App.route('/', methods=['GET'])
+def index():
+    """ Displays the index page accessible at '/' """
+
+    return render_template('index.html')
+
+@Flask_App.route('/operation_result/', methods=['POST'])
+def operation_result():
+    """Route where we send calculator form input"""
+
+    #error = None
+    result = None
+
+    # request.form looks for:
+    # html tags with matching "name= "
+    first_input = request.form['Input1']  
+    second_input = request.form['Input2']
+    operation = request.form['operation']
+
+    try:
+        input1 = float(first_input)
+        input2 = float(second_input)
+
+        # On default, the operation on webpage is addition
+        if operation == "+":
+            result = input1 + input2
+
+        elif operation == "-":
+            result = input1 - input2
+
+        elif operation == "/":
+            result = input1 / input2 
+
+        elif operation == "*":
+            result = input1 * input2
+
+        else:
+            operation = "%"
+            result = input1 % input2
+
+        return render_template(
+            'index.html',
+            input1=input1,
+            input2=input2,
+            operation=operation,
+            result=result,
+            calculation_success=True
+        )
+        
+    except ZeroDivisionError:
+        return render_template(
+            'index.html',
+            input1=input1,
+            input2=input2,
+            operation=operation,
+            result="Bad Input",
+            calculation_success=False,
+            error="You cannot divide by zero"
+        )
+        
+    except ValueError:
+        return render_template(
+            'index.html',
+            input1=first_input,
+            input2=second_input,
+            operation=operation,
+            result="Bad Input",
+            calculation_success=False,
+            error="Cannot perform numeric operations with provided input"
+        )
+
+if __name__ == '__main__':
+    Flask_App.debug = True
+    Flask_App.run()
